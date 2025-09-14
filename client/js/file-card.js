bbfdbd92aa868c3ba8c347fee8a4951e398d8a7b
Marina <m.jacquet@eggon.fr>
2025-09-15 00:08:26 +0200

    Update file-card.js

--- client/js/file-card.js
+++ client/js/file-card.js
@@ -197,15 +197,6 @@ class FileCard extends BaseCard {
         };
     }
 
-    loadScript(src) {
-        return new Promise((resolve, reject) => {
-            const script = document.createElement('script');
-            script.src = src;
-            script.onload = resolve;
-            script.onerror = reject;
-            document.head.appendChild(script);
-        });
-    }
 
     getHTML() {
         const actions = [
